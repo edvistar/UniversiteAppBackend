5ccc9d31b99635ae68b92d41999485d4ff1362b7
esvi saho <esvi210@hotmail.com>
2023-05-13 23:14:31 -0500

    Maximo caracteres propiedad Nombre clase Curso

--- Models/DataModels/Curso.cs
+++ Models/DataModels/Curso.cs
@@ -4,12 +4,14 @@ namespace UniversiteAppBackend.Models.DataModels
 {
     public class Curso: BaseEntity
     {
+        [Required, MaxLength(50)]
         public string Nombre { get; set; } = string.Empty;
         [Required, MaxLength(280)]
         public string DescripcionCorta { get; set; } = string.Empty;
         public string DescripcionLarga { get; set; } = string.Empty;
         public string PublicoObjetivo { get; set; } = string.Empty;
         public string Objetivos { get; set; } = string.Empty;
+        public string Requisitos { get; set; } = string.Empty;
         public enum Nivel { Basico, Intermedio, Avanzado }
 
     }
